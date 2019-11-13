ee6cf1334a9036611f87e2c4ecaed8c13dc5e006
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-13 23:16:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{:railroads => {}}
+	r = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
@@ -9,14 +9,14 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-	{:railroads => {
+	r = {:railroads => {
 		:pieces => 4
 		}
 	}
 end
 
 def monopoly_with_third_tier
-	{:railroads => {
+	r = {:railroads => {
 		:pieces => 4,
 		:rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
 		:names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	{:railroads => {
+	r = {:railroads => {
 		:pieces => 4,
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
@@ -39,18 +39,18 @@ def monopoly_with_fourth_tier
 		},
 		:names => {
 			:reading_railroad => {
-				:mortgage_value => "$" + 100
-				},
+				  :mortgage_value => "$" + 100
+				  },
 			:pennsylvania_railroad => {
 					:mortgage_value => "$" + 100
 					},
 			:b_and_o_railroad => {
-						:mortgage_value => "$" + 100
-						},
+					:mortgage_value => "$" + 100
+					},
 			:shortline_railroad => {
-							:mortgage_value => "$" + 100
-							}
-						}
+					:mortgage_value => "$" + 100
+					}
+						 }
 		}
 	}
 
