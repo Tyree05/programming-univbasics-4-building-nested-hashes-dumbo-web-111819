483e23d8eb25152c226fd295134f7f1aac4ddda3
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-13 23:46:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
       },
   :names => {
 		:reading_railroad => {
-		  "mortgage_value" => "$100"
+		  "mortgage_value" => $100
 		  },
 	  :pennsylvania_railroad => {
 	    :mortgage_value => "$100"
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 		}
 	}
 }
-[:railroads][:names][:reading_railroad][:mortgage_value]
+[:railroads][:names][:reading_railroad]["mortgage_value"]
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
