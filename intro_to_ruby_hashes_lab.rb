4cd41fcbc3a34ac760d2793646d5f1b5de7df703
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-13 23:27:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ r = {:railroads => {
 						}
 		}
 	}
-return r[:railroads][:names][:reading_railroad]
+return r[:railroads][:names][:reading_railroad][:mortgage_value]
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
